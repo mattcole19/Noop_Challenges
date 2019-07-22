f165751e368e3086d7d117102157fdc828bfb9c2
Matt Cole <ccmatt19@gmail.com>
2019-07-21 20:37:46 -0400

    GUI now has all necessary elements

--- mazebot/simpler_GUI.py
+++ mazebot/simpler_GUI.py
@@ -6,34 +6,31 @@ class UserInterface(Frame):
     def __init__(self, master):
         super().__init__()
         self.master = master
+        self.master.geometry('500x500')
 
         # Variables that will be needed by the maze
         self.min_value = 0
         self.max_value = 100
         self.paths = []
 
-        # Just some colors
-        self.bg = 'black'
-        self.fg = 'white'
-
-        # Title
+        # Window Title
         self.master.title('Maze Solver')
 
-        # Minimum size
+        # Minimum size for maze
         self.min_label = Label(master=master, text='Choose the minimum size of the maze')
         self.min_label.pack()
 
         self.min_slider = Scale(orient='horizontal', from_=0, to=100)
         self.min_slider.pack()
 
-        # Maximum size
+        # Maximum size for maze
         self.max_label = Label(master=master, text='Choose the maximum size of the maze')
         self.max_label.pack()
 
         self.max_slider = Scale(orient='horizontal', from_=0, to=100)
         self.max_slider.pack()
 
-        # Checkboxes
+        # Checkboxes for which paths to show
         self.path_label = Label(master=master, text='Choose which paths you would like to see')
         self.path_label.pack()
 
@@ -44,7 +41,7 @@ class UserInterface(Frame):
         self.full_path.pack()
 
         # Start button
-        self.start = Button(master=master, text='START')
+        self.start = Button(master=master, text='START', command=self.start)
         self.start.pack()
 
     def start(self):
@@ -52,12 +49,8 @@ class UserInterface(Frame):
         Starts maze solver
         :return:
         """
+        self.min_value = self.min_slider.get()
+        self.max_value = self.max_slider.get()
 
-        pass
-
-
-root = Tk()
-ui = UserInterface(root)
-
-root.geometry('500x500')
-root.mainloop()
+        self.
+        print('button pressed')
